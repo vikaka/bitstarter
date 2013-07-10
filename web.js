2462762f6af8ca57fe70d982b6bf8fd21bcd314e
Ubuntu <ubuntu@ip-172-31-13-193.us-west-2.compute.internal>
2013-07-10 02:51:49 +0000

    updated web.js to read index

--- web.js
+++ web.js
@@ -7,7 +7,8 @@ app.get('/', function(request, response) {
   response.send(fs.readFileSync(index.html));
 console.log(json);
 });
-
+var copy = new Buffer(JSON.parse(json));
+console.log(copy);
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
