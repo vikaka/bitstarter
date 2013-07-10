13e1f6c63b0ea58e295326ac7e35e67d3384dc4b
Ubuntu <ubuntu@ip-172-31-13-193.us-west-2.compute.internal>
2013-07-10 03:14:41 +0000

    updated web.js to read index.html

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 
 var fs = require('fs');
-var buf fs.readFileSync('index.html', 'utf-8');
+var buf = fs.readFileSync("index.html", "utf-8");
   response.send(buf.toString());
 
 });
