a8f949507f13032dbe0b8bf18a8f6a386e34f6cd
Ubuntu <ubuntu@ip-172-31-13-193.us-west-2.compute.internal>
2013-07-10 02:34:23 +0000

    updated web to read index

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var buf = new buffer('index');
+var json = JSON.stringlify(buf);
 var app = express.createServer(express.logger());
 var fs = require('fs');
 app.get('/', function(request, response) {
   response.send(fs.readFileSync(index.html));
+console.log(json);
 });
 
 var port = process.env.PORT || 5000;
