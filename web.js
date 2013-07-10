32e322a60bbf0295715f406200f3744a82200b38
Ubuntu <ubuntu@ip-172-31-13-193.us-west-2.compute.internal>
2013-07-10 04:26:51 +0000

    updated web.js for index.html

--- web.js
+++ web.js
@@ -5,8 +5,8 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 
 var fs = require('fs');
-var content = fs.readFileSync("index.html", "utf-8");
-  response.send(content);
+var data = fs.readFileSync("index.html", "utf-8");
+  response.send(data);
 
 });
 
