7d8d5bd32378ff8b452ca161ec244a2e3ab57a67
vikaka <ubuntu@ip-172-31-31-216.us-west-2.compute.internal>
2013-08-21 12:46:30 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,16 +1,18 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-
 var fs = require('fs');
-var data = fs.readFileSync("index.html", "utf-8");
-  response.send(data);
+var htmlfile = "index.html";
 
-});
-
-var port = process.env.PORT || 5000;
+var app = express.createServer(express.logger());
+app.get('/', function(request,response) 
+{
+var html = fs.readFileSync(htmlfile).toString();
+response.send(html);
+app.get('/login', function(req, res){
+   res.send(fs.readFileSync('form.html').toString());
+}); });
+  
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
